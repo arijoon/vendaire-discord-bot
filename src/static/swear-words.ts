50087ec1038ef13cf25f9b4492fec6ebf0370c5c
Arman Yaraee <arman@yaraee.net>
2017-04-22 12:15:16 +0100

    Improved with new phrases

--- src/static/swear-words.ts
+++ src/static/swear-words.ts
@@ -1,5 +1,6 @@
 let swearWords = [ 'fucking asshole', 'fucking moron', 'piece of shit', 'cocksucker', 'little rat', 'twat', 'cunt', 'fucking cunt', 
-    'bender'
+    'bender', 'disgusting jew', 'bloody leaf', 'insufferable abomonation of humanity', '- may allah forgive me for saying this word - leaf',
+    'absolute waste of space'
 ];
 
 export { swearWords }
