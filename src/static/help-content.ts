1c4de9090750c3df1f200b1658054451ed885782
Arman Yaraee <arman@yaraee.net>
2017-04-24 22:26:18 +0100

    Updated help command

--- src/static/help-content.ts
+++ src/static/help-content.ts
@@ -11,12 +11,22 @@ indent.inc();
 helpContent.usage += `${indent}Commands:\n\n`
 
 indent.inc();
-helpContent.usage += `${indent}help| --help\tshow this prompt\n`
-helpContent.usage += `${indent}trump       \tPlay glorious trump audio in your current voice channel\n`
-helpContent.usage += `${indent}tfw         \tpost a feel pic\n`
-helpContent.usage += `${indent}bog         \tpost a bog pic\n`
-helpContent.usage += `${indent}tsu         \tpost a tsuuuu pic\n`
-helpContent.usage += `${indent}god         \tpost a god pic (usually trump) pic\n`
-helpContent.usage += `${indent}quickrundown\tpost quickrundown pic with description\n`
+helpContent.usage += `${indent}help| --help            \tShow this prompt\n`
+helpContent.usage += `${indent}trump                   \tPlay trump audio in your current voice channel\n`
+helpContent.usage += `${indent}qs                      \tPost a random question\n`
+helpContent.usage += `${indent}yt {text}               \tPost first result of youtube search for text\n`
+helpContent.usage += `${indent}tfw                     \tPost a feel pic\n`
+helpContent.usage += `${indent}bog                     \tPost a bog pic\n`
+helpContent.usage += `${indent}tsu                     \tPost a tsuuuu pic\n`
+helpContent.usage += `${indent}god                     \tPost a god pic\n`
+helpContent.usage += `${indent}call                    \tPost a call pic\n`
+helpContent.usage += `${indent}exposed                 \tPost an exposed pic\n`
+helpContent.usage += `${indent}lol                     \tPost a lol pic \n`
+helpContent.usage += `${indent}quickrundown            \tPost quickrundown pic with description\n`
+helpContent.usage += `${indent}regional {text}         \tPost your text in regional format\n`
+helpContent.usage += `${indent}spaceout {text}         \tSpace out each character and add column\n`
+helpContent.usage += `${indent}is{somthing} {text}     \tRandomize whether something is text\n`
+helpContent.usage += `${indent}are{somethings} {text}  \tRandomize whether something are text\n`
+helpContent.usage += `${indent}does{somethings} {text} \tRandomize whether something does text\n`
 
 export { helpContent }
