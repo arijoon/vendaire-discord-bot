3a2769e6892d157bb13a47092542b975d36541f4
Arman Yaraee <arman@yaraee.net>
2017-05-04 20:08:55 +0100

    Added Imdb search command

---
+++ src/commands/imdb.ts
@@ -0,0 +1,56 @@
+import {ICache} from '../contracts/ICache';
+import { IDisposable } from 'rx';
+import { IClient } from '../contracts/IClient';
+import { injectable, inject } from 'inversify';
+import { ICommand } from './../contracts/ICommand';
+import { TYPES } from "../ioc/types";
+import { commands } from "../static/commands";
+
+import * as path from 'path';
+import * as imdb from 'imdb-api';
+
+@injectable()
+export class ImdbCommand implements ICommand {
+
+    _banCommand: string = commands.imdb;
+    _subscription: IDisposable[] = [];
+
+    constructor(
+        @inject(TYPES.IClient) private _client: IClient,
+        @inject(TYPES.ICacheString) private _cache: ICache<string, any>
+    ) { }
+
+    attach(): void {
+        this._subscription.push(this._client
+            .getCommandStream(this._banCommand)
+            .subscribe(imsg => {
+                let msg = imsg.Message;
+
+                const content = msg.content.trim();
+                if (!content) {
+                    imsg.done();
+                    return;
+                }
+
+                let res;
+                if (this._cache.has(content)) {
+                    res = msg.channel.send(this._cache.getType<string>(content));
+
+                } else {
+
+                    res = imdb.get(content)
+                        .then(res => {
+                            let url = res.imdburl;
+                            return msg.channel.send(url);
+                        });
+                }
+
+                res.then(_ => {
+                    imsg.done();
+                }).catch(err => {
+                    msg.channel.send(`, ${content} not found mofo`, { reply: msg });
+                    imsg.done(err, true);
+                });
+            }));
+    }
+}
