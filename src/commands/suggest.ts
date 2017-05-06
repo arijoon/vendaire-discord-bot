52498d6dc4858f6b82c27665fea0badf92613c67
Arman Yaraee <arman@yaraee.net>
2017-05-06 21:13:42 +0100

    Added suggest command

---
+++ src/commands/suggest.ts
@@ -0,0 +1,52 @@
+import { IHttp } from '../contracts/IHttpService';
+import { IConfig } from '../contracts/IConfig';
+import { IDisposable } from 'rx';
+import { IClient } from '../contracts/IClient';
+import { injectable, inject } from 'inversify';
+import { ICommand } from './../contracts/ICommand';
+import { TYPES } from "../ioc/types";
+import { commands } from "../static/commands";
+
+@injectable()
+export class SuggestCommand implements ICommand {
+
+    _command: string = commands.suggest;
+    _subscription: IDisposable[] = [];
+
+    constructor(
+        @inject(TYPES.IClient) private _client: IClient,
+        @inject(TYPES.IHttp) private _http: IHttp,
+        @inject(TYPES.IConfig) private _config: IConfig
+    ) { }
+
+    attach(): void {
+        this._subscription.push(this._client
+            .getCommandStream(this._command)
+            .subscribe(imsg => {
+                let msg = imsg.Message;
+
+                const content = msg.content.trim();
+                if (!content) {
+                    imsg.done("Empty message", true);
+                    return;
+                }
+
+                let url = `http://suggestqueries.google.com/complete/search?client=firefox&q=${content}`;
+
+                this._http.get(url)
+                    .then(res => {
+                        let results = JSON.parse(`{"data":${res}}`).data;
+
+                        res = "Suggestions:\n"
+                        for (let item of results[1]) {
+                            res += `\t - ${item}\n`;
+                        }
+
+                        return msg.channel.send(res, { code: 'md' });
+                    }).then(_ => imsg.done())
+                    .catch(err => {
+                        imsg.done(err, true);
+                    });
+            }));
+    }
+}
