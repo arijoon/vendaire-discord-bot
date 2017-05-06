a236117341be92271c1e3efce6c62cea14e8db40
Arman Yaraee <arman@yaraee.net>
2017-05-06 10:20:00 +0100

    Fixed helpers, added static colors and prettified translate

--- README.md
+++ README.md
@@ -16,12 +16,41 @@ npm start
 ### Commands
 
 ```
-bog - posts a random picture from bog folder in images
-tfw - posts a random picture from tfw folder in images
-tsu - posts a random picture from tsu folder in images
-trump - plays a random trump audio file in the current chat channel you are
-quickrundown - posts quickrundown.txt content prefixed with quickrundown.png in the bog folder
-help - shows help
+Usage: !![commands] [optional arguments]
+
+    Commands:
+
+        help| --help                     Show this prompt
+        trump                            Play trump audio in your current voice channel
+        qs                               Post a random question
+        yt {text}                        Post first result of youtube search for text
+        tfw                              Post a feel pic
+        bog                              Post a bog pic
+        tsu                              Post a tsuuuu pic
+        god                              Post a god pic
+        call                             Post a call pic
+        exposed                          Post an exposed pic
+        lol                              Post a lol pic 
+        bullshit                         Post a lol pic 
+        memri                            Post a lol pic 
+        yousmart                         Post a lol pic 
+        randomPic                        Post a lol pic 
+        quickrundown                     Post quickrundown pic with description
+        regional {text}                  Post your text in regional format
+        spaceout {text}                  Space out each character and add column
+        rate {name|link}                 Rates the subject out of 10
+        math {expression}                Evaluates the mathematical expression
+        is{somthing} {text}              Randomize whether something is text
+        are{somethings} {text}           Randomize whether something are text
+        does{somethings} {text}          Randomize whether something does text
+        will{somthing} {text}            Randomize whether something is text
+        would{somethings} {text}         Randomize whether something are text
+        has{somethings} {text}           Randomize whether something does text
+        have{somethings} {text}          Randomize whether something does text
+        whosonline                       Show who is online
+        clean {num}                      clean the last {num} bot messages
+        imdb {moviename} [options]       Search imdb for moviename
+        translate {text} [options]       translate the text
 ```
 
 
@@ -46,19 +75,13 @@ help - shows help
              cons.forEach((v, k) => v.disconnect()); // loops through and executes dc
          });
  }
-
- public detach(): void {
-     this._subscription.dispose(); // detaches the subscription
- }
-
 ```
 
 
 
 The above class implements `ICommand` and is binded in the `container.ts` . Simply bind any new `ICommand` instance and attach to the stream you wish to intercept. It'll work like magic. 
 
-
-
 ## Entry point
 
 The entry is in `bootstrap.ts`, so read from there if you want a complete grasp
+The prod entry point is in `cluster.ts`, the clustering system is used to take advantage of multithreading and process all commands truely as parallel as each other without interference
--- src/static/help-content.ts
+++ src/static/help-content.ts
@@ -11,24 +11,36 @@ indent.inc();
 helpContent.usage += `${indent}Commands:\n\n`
 
 indent.inc();
-helpContent.usage += `${indent}help| --help            \tShow this prompt\n`
-helpContent.usage += `${indent}trump                   \tPlay trump audio in your current voice channel\n`
-helpContent.usage += `${indent}qs                      \tPost a random question\n`
-helpContent.usage += `${indent}yt {text}               \tPost first result of youtube search for text\n`
-helpContent.usage += `${indent}tfw                     \tPost a feel pic\n`
-helpContent.usage += `${indent}bog                     \tPost a bog pic\n`
-helpContent.usage += `${indent}tsu                     \tPost a tsuuuu pic\n`
-helpContent.usage += `${indent}god                     \tPost a god pic\n`
-helpContent.usage += `${indent}call                    \tPost a call pic\n`
-helpContent.usage += `${indent}exposed                 \tPost an exposed pic\n`
-helpContent.usage += `${indent}lol                     \tPost a lol pic \n`
-helpContent.usage += `${indent}quickrundown            \tPost quickrundown pic with description\n`
-helpContent.usage += `${indent}regional {text}         \tPost your text in regional format\n`
-helpContent.usage += `${indent}spaceout {text}         \tSpace out each character and add column\n`
-helpContent.usage += `${indent}rate {name|link}        \tRates the subject out of 10\n`
-helpContent.usage += `${indent}math {expression}       \tEvaluates the mathematical expression\n`
-helpContent.usage += `${indent}is{somthing} {text}     \tRandomize whether something is text\n`
-helpContent.usage += `${indent}are{somethings} {text}  \tRandomize whether something are text\n`
-helpContent.usage += `${indent}does{somethings} {text} \tRandomize whether something does text\n`
+helpContent.usage += `${indent}help| --help                 \tShow this prompt\n`
+helpContent.usage += `${indent}trump                        \tPlay trump audio in your current voice channel\n`
+helpContent.usage += `${indent}qs                           \tPost a random question\n`
+helpContent.usage += `${indent}yt {text}                    \tPost first result of youtube search for text\n`
+helpContent.usage += `${indent}tfw                          \tPost a feel pic\n`
+helpContent.usage += `${indent}bog                          \tPost a bog pic\n`
+helpContent.usage += `${indent}tsu                          \tPost a tsuuuu pic\n`
+helpContent.usage += `${indent}god                          \tPost a god pic\n`
+helpContent.usage += `${indent}call                         \tPost a call pic\n`
+helpContent.usage += `${indent}exposed                      \tPost an exposed pic\n`
+helpContent.usage += `${indent}lol                          \tPost a lol pic \n`
+helpContent.usage += `${indent}bullshit                     \tPost a lol pic \n`
+helpContent.usage += `${indent}memri                        \tPost a lol pic \n`
+helpContent.usage += `${indent}yousmart                     \tPost a lol pic \n`
+helpContent.usage += `${indent}randomPic                    \tPost a lol pic \n`
+helpContent.usage += `${indent}quickrundown                 \tPost quickrundown pic with description\n`
+helpContent.usage += `${indent}regional {text}              \tPost your text in regional format\n`
+helpContent.usage += `${indent}spaceout {text}              \tSpace out each character and add column\n`
+helpContent.usage += `${indent}rate {name|link}             \tRates the subject out of 10\n`
+helpContent.usage += `${indent}math {expression}            \tEvaluates the mathematical expression\n`
+helpContent.usage += `${indent}is{somthing} {text}          \tRandomize whether something is text\n`
+helpContent.usage += `${indent}are{somethings} {text}       \tRandomize whether something are text\n`
+helpContent.usage += `${indent}does{somethings} {text}      \tRandomize whether something does text\n`
+helpContent.usage += `${indent}will{somthing} {text}        \tRandomize whether something is text\n`
+helpContent.usage += `${indent}would{somethings} {text}     \tRandomize whether something are text\n`
+helpContent.usage += `${indent}has{somethings} {text}       \tRandomize whether something does text\n`
+helpContent.usage += `${indent}have{somethings} {text}      \tRandomize whether something does text\n`
+helpContent.usage += `${indent}whosonline                   \tShow who is online\n`
+helpContent.usage += `${indent}clean {num}                  \tclean the last {num} bot messages\n`
+helpContent.usage += `${indent}imdb {moviename} [options]   \tSearch imdb for moviename\n`
+helpContent.usage += `${indent}translate {text} [options]   \ttranslate the text\n`
 
 export { helpContent }
