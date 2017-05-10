f9b6981bf30bce1792acf4c41bfc5a1d9224ef87
Arman Yaraee <arman@yaraee.net>
2017-05-10 20:18:00 +0100

    Added disconnect procedure to reconnect

---
+++ src/components/timer-queue.com.ts
@@ -0,0 +1,60 @@
+import { IDisposable } from 'rx';
+
+/** Queues tasks and only perform them once every x seconds */
+export class  TimerQueue {
+
+    private _timeout: number;
+    private _tasks: Map<IDisposable, Function>;
+    private _queue: IDisposable[];
+    private _busy = false;
+
+    constructor(timeout: number = 1000) {
+        this._timeout = timeout;
+    }
+
+    doTask(task: () => void) {
+        if(!this._busy) {
+            this._busy = true;
+            task();
+
+            setTimeout(() => this.update(), this._timeout);
+            return;
+        } 
+
+        let d = new DisposableTask(() => this._tasks.delete(d));
+
+        this._tasks.set(d, task);
+        this._queue.unshift(d);
+    }
+
+    setNewTimeout(timeout: number) {
+        this._timeout = timeout;
+    }
+
+    private update() {
+        if(this._tasks.size > 0) {
+            let d = this._queue.pop();
+
+            this._tasks.get(d)();
+            d.dispose();
+
+            setTimeout(() => this.update(), this._timeout);
+
+            return;
+        } 
+
+        this._busy = false;
+
+    }
+}
+
+class DisposableTask implements IDisposable {
+    private _onDispose;
+    constructor(onDispose: () => void) {
+        this._onDispose = onDispose;
+    }
+
+    dispose(): void {
+        this._onDispose();
+    }
+}
