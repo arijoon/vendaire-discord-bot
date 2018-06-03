f71708b150df08d3480dde1b9e117125c75f7754
Arijoon <arman@somewhere.com>
2018-06-03 00:37:57 +0000

    Updated update script

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
-docker image build --tag discord-bot:$(git describe) .  
-docker container rm -f discord-bot
-docker run -d -v /home/pi/prod/discord/assets:/app/assets --restart=always --name discord-bot discord-bot:$(git describe)
+docker-compose build
+docker-compose down
+docker-compose up -d
