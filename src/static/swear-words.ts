87b6db7dac7675488601906c1769ad6b47813517
Arman Yaraee <arman@yaraee.net>
2018-06-07 23:50:34 +0100

    Minor cleanup in package.json

--- src/static/swear-words.ts
+++ src/static/swear-words.ts
@@ -1,6 +1,6 @@
 let swearWords = [ 'fucking asshole', 'fucking moron', 'piece of shit', 'cocksucker', 'little rat', 'twat', 'cunt', 'fucking cunt', 
-    'bender', 'disgusting jew', 'bloody leaf', 'insufferable abomonation of humanity', '- may allah forgive me for saying this word - leaf',
-    'absolute waste of space'
+    'bender', 'disgusting rat', 'bloody leaf', 'insufferable abomonation of humanity', '- may allah forgive me for saying this word - leaf',
+    'absolute waste of space', 'biggest wanker of the century', 'scum amongst humanity'
 ];
 
 export { swearWords }
