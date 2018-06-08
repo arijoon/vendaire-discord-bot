073923bc493cd9288d57a556bec58f0c8e812e5a
Arman Yaraee <arman@yaraee.net>
2018-06-09 00:28:21 +0100

    Merged config files

--- src/commands/youtube.command.ts
+++ src/commands/youtube.command.ts
@@ -18,7 +18,7 @@ export class YoutubeSearch implements ICommand {
         @inject(TYPES.IConfig) private _config: IConfig
     ){
         this._yt = new Youtube();
-        this._yt.setKey(_config.secret.youtube.key)
+        this._yt.setKey(_config.app.youtube.key)
     }
 
     attach(): void {
