1d2f8e87c42c009cc4cd46224bb38b9962a3dc95
Arman Yaraee <arman@yaraee.net>
2018-06-08 23:48:05 +0100

    Removed multi threading logic

--- src/contracts/IProcess..ts
+++
@@ -1,9 +0,0 @@
-import { DiscordMessage } from './../models/discord-message';
-import { IObservable } from 'rx';
-
-export interface IProcess extends IStartable {
-    IsActive: boolean;
-    MessagesStream: IObservable<DiscordMessage>;
-
-    ready(): void;
-}
