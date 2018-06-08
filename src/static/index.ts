cd817b7ba610b0dcbf5e09765b762f872dc4d011
Arman Yaraee <arman@yaraee.net>
2018-06-08 16:20:42 +0100

    Unified index structure in static data

--- src/static/index.ts
+++ src/static/index.ts
@@ -1,12 +1,5 @@
-/**
- * This index file is different to others
- * import and manually export
- */
-
-import { colors } from './colors';
-import { commands } from './commands';
-import { langs, getCode, isSupported } from './languages';
-import { swearWords } from './swear-words';
-import { helpContent } from './help-content';
-
-export { colors, commands, swearWords, helpContent, langs, getCode, isSupported };
+export  * from  './colors';
+export  * from  './commands';
+export  * from  './languages';
+export  * from  './swear-words';
+export  * from  './help-content';
