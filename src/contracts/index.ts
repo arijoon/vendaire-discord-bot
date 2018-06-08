29a342c915e9f8af62086b4fdfee0c221cb4ce86
Arman Yaraee <arman@yaraee.net>
2018-06-08 23:51:24 +0100

    Removed multi threading dependencies

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -2,7 +2,5 @@ export * from "./IAudioPlayer";
 export * from "./IClient";
 export * from "./IMessage";
 export * from "./IMessageUtils";
-export * from "./IProcess.";
-export * from "./IProcessManager";
 export * from "./IQuestionService";
 export * from "./images/IFont";
