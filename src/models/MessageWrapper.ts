539c788fbf6e1561039a16221ce71d30f400b114
Arman Yaraee <arman@yaraee.net>
2018-06-11 04:30:08 +0100

    Renamed message wrapper, added done promise

--- src/models/MessageWrapper.ts
+++ src/models/MessageWrapper.ts
@@ -11,6 +11,9 @@ export class MessageWrapper implements IMessage {
   public readonly userId: string;
   public readonly guidId: string;
   public readonly channelId: string;
+  public readonly onDone: Promise<{ msg?: string; err?: any; }>;
+
+  private _onDoneResolver: (reso: { msg?: string, err?: any}) => void;
 
   constructor(
     private _onDone: (msg?: string, err?: any) => void,
@@ -28,10 +31,13 @@ export class MessageWrapper implements IMessage {
     this.guidId = msg.guild.id;
     this.channelId = msg.channel.id;
     this.userId = msg.author.id;
+
+    this.onDone = new Promise(r => this._onDoneResolver = r);
   }
 
   done(msg?: string, err?: any): void {
     this._onDone(msg, err);
+    this._onDoneResolver({msg, err});
   }
 
   send(content?: string, options?: any): Promise<Message | Message[]> {
---
+++ src/monitoring/README.md
@@ -0,0 +1,3 @@
+# Monitoring
+
+This section contains logic for monitoring and providing interfaces for interacting with the bot through a web interface
