9daac46cbefc7fbe3f70dadc25db78b01587f295
Arman Yaraee <arman@yaraee.net>
2018-06-12 22:25:05 +0100

    Changed the random selection in random pic to be less random and reduce duplicates

--- src/commands/random-pic.command.ts
+++ src/commands/random-pic.command.ts
@@ -7,6 +7,8 @@ import { commands } from '../static';
 import * as path from 'path';
 import { Message, MessageAttachment } from 'discord.js';
 
+const RandomRange: number = 1/20;
+
 @injectable()
 export class RandomPic implements ICommand {
 
@@ -66,8 +68,23 @@ export class RandomPic implements ICommand {
     return this._filesService
       .getAllFiles(fullPath)
       .then(lst => {
-        return this._config.pathFromRoot(fullPath, lst.crandom());
-
+        return this._config.pathFromRoot(fullPath, this.getRandom(lst, dir));
       });
+  }
+
+  _lastRandomInx: { [key: string]: number } = {};
+  /**
+   * generates a random by adding a number (using RandomRange*lst.lenght) to the last index selection
+   * @param lst list to pick a random from
+   */
+  private getRandom(lst: string[], key: string) : string {
+    if(!this._lastRandomInx[key]) {
+      this._lastRandomInx[key]  = Math.floor(Math.random() * lst.length);
+    }
+
+    const range = RandomRange * lst.length;
+    this._lastRandomInx[key] = (this._lastRandomInx[key] + (Math.ceil(Math.random() * range))) % lst.length;
+    
+    return lst[this._lastRandomInx[key]];
   }
 }
