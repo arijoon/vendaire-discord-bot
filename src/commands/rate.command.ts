4cd4c971338155381dc666f4af49f1d7171eb83e
Arman Yaraee <arman@yaraee.net>
2018-06-12 21:00:01 +0100

    Fixed bug in rate command after folder restructure

--- src/commands/rate.command.ts
+++ src/commands/rate.command.ts
@@ -10,67 +10,68 @@ import * as path from 'path';
 @injectable()
 export class RateCommand implements ICommand {
 
-    readonly _command: string = commands.rate;
+  readonly _command: string = commands.rate;
 
-    readonly _chances = {
-        1: "would not touch with a stick",
-        2: "would not touch",
-        9: "would like to decimate",
-        10: "would absolutely decimate uncontrollably"
-    };
-    
-    constructor(
-        @inject(TYPES.IClient) private _client: IClient,
-        @inject(TYPES.IConfig) private _config: IConfig,
-        @inject(TYPES.IFiles) private _filesService: IFiles
-    ) { }
+  readonly _chances = {
+    1: "would not touch with a stick",
+    2: "would not touch",
+    8: "would like to decimate",
+    9: "would absolutely decimate uncontrollably",
+    10: "would drag my balls across broken glass to hear her fart through a walky talky"
+  };
 
-    attach(): void {
-        this._client
-            .getCommandStream(this._command)
-            .subscribe(imsg => {
-                const msg = imsg.Message;
+  constructor(
+    @inject(TYPES.IClient) private _client: IClient,
+    @inject(TYPES.IConfig) private _config: IConfig,
+    @inject(TYPES.IFiles) private _filesService: IFiles
+  ) { }
 
-                let roll = this.getRoll(3);
+  attach(): void {
+    this._client
+      .getCommandStream(this._command)
+      .subscribe(imsg => {
+        const msg = imsg.Message;
 
-                let result: string;
-                let options: any = {};
+        let roll = this.getRoll(3);
 
-                if(commonRegex.link.test(msg.content.trim())) {
-                    options.file = commonRegex.link.exec(msg.content)[0];
-                    result = `This is ${roll}/10 `
-                } else {
-                    result = `${msg.content} is ${roll}/10 `
-                }
+        let result: string;
+        let options: any = {};
 
-                if (this._chances[roll])
-                    result += this._chances[roll]
-
-                msg.channel.send(result, options)
-                    .then(() => imsg.done())
-                    .catch(err => imsg.done());
-
-                if (roll > 8) {
-                    const dirPath = path.join(this._config.images["root"], this._config.images['fap']);
-
-                    this._filesService.getRandomFile(dirPath)
-                        .then(file => {
-
-                            const filePath = this._config.pathFromRoot(dirPath, file);
-                            msg.channel.send('', { file: filePath })
-                        });
-                }
-            });
-    }
-
-    getRoll(tries: number): number {
-        let highest = 0;
-        for(let i = 0; i < tries; i++) {
-            let roll = Math.floor(Math.random()*10);
-
-            if(roll > highest) highest = roll
+        if (commonRegex.link.test(msg.content.trim())) {
+          options.file = commonRegex.link.exec(msg.content)[0];
+          result = `This is ${roll}/10 `
+        } else {
+          result = `${msg.content} is ${roll}/10 `
         }
 
-        return highest;
+        if (this._chances[roll])
+          result += this._chances[roll]
+
+        msg.channel.send(result, options)
+          .then(() => imsg.done())
+          .catch(err => imsg.done());
+
+        if (roll > 8) {
+          const dirPath = path.join(this._config.images["root"], commands.randomPic, 'fap');
+
+          this._filesService.getRandomFile(dirPath)
+            .then(file => {
+
+              const filePath = this._config.pathFromRoot(dirPath, file);
+              msg.channel.send('', { file: filePath })
+            });
+        }
+      });
+  }
+
+  getRoll(tries: number): number {
+    let highest = 0;
+    for (let i = 0; i < tries; i++) {
+      let roll = Math.floor(Math.random() * 10);
+
+      if (roll > highest) highest = roll
     }
+
+    return highest;
+  }
 }
