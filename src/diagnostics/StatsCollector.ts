dfc37c83160a00703d7dd6ff8f3c11513b011cb1
Arman Yaraee <arman@yaraee.net>
2018-11-24 15:51:01 +0000

    Added server and stats collector

---
+++ src/diagnostics/StatsCollector.ts
@@ -0,0 +1,32 @@
+import { IStatsCollector } from "./IStatsCollector";
+import { injectable, inject } from "inversify";
+import { TYPES } from "../ioc/types";
+import { timeInSeconds } from "./buckets";
+
+import * as prom from 'prom-client';
+
+@injectable()
+export class StatsCollector implements IStatsCollector {
+  hists: {[key: string]: prom.Histogram} = {};
+  keys = {
+    responseTime: 'response-time'
+  };
+
+  constructor(
+    @inject(TYPES.IConfig) private _config: IConfig,
+    @inject(TYPES.Logger) private _logger: ILogger,
+  ) {
+    this.hists[this.keys.responseTime] = new prom.Histogram({
+      name: 'command_duration_seconds',
+      help: 'Duration of each command in seconds',
+      labelNames: ['command'],
+      buckets: timeInSeconds
+    });
+   }
+
+  collectResponseTime(value: number, command: string) {
+    this.hists[this.keys.responseTime]
+      .labels(command)
+      .observe(value);
+  }
+}
