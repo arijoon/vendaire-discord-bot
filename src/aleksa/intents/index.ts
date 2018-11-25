5b9cb4155a54db1d9b8eca203338c34849966dff
Arman Yaraee <arman@yaraee.net>
2018-11-25 15:10:03 +0000

    Added alexa capability to play tachanka

--- src/aleksa/intents/index.ts
+++ src/aleksa/intents/index.ts
@@ -2,5 +2,6 @@ export * from "./change-server.intent";
 export * from "./play-trump.intent";
 export * from "./post-image.intent";
 export * from "./query-server.intent";
-export * from "./tfw.intent";
 export * from "./replay-intent";
+export * from "./tachanka-bot-intent";
+export * from "./tfw.intent";
