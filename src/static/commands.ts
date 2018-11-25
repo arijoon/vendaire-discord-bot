ccec53f70949515acf10b454ed098cd2294309bb
Arman Yaraee <arman@yaraee.net>
2018-11-25 14:54:40 +0000

    Updated commands, added bst

--- src/static/commands.ts
+++ src/static/commands.ts
@@ -13,7 +13,7 @@ let commands = {
     // Dependencies: rate, addpic
     randomPics: [ 'tfw', 'god', 'bog', 'exposed', 'tsu', 'lol', 'fap', 'call', 'bullshit', 'memri', 'yousmart', 'ungabunga', 'kys', 'misc'
      ,'tekken', 'todd', 'anime-legacy', 'anime', 'beargrylls', 'indian', 'dafuq'
-     ,'umaru', 'boomer', 'zoomer', 'anims', 'animr', 'pepe' // requests
+     ,'umaru', 'boomer', 'zoomer', 'pepe', 'bst' // requests
      ,'sfv', 'blazbluecf', 'kpop', 'games', 'movies'
      ,'nsfw' // special case, will post it to the NSFW channel
      ,"trash" // for removed items
