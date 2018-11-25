48e58e5c6d1c53fc25dceab06abab991f0aa5e68
Arman Yaraee <arman@yaraee.net>
2018-11-25 15:10:03 +0000

    Added alexa capability to play tachanka

---
+++ src/aleksa/intents/tachanka-bot-intent.ts
@@ -0,0 +1,24 @@
+import { ServerSelectorService } from './../server-selector.service';
+import { injectable, inject } from 'inversify';
+import { TYPES } from '../../ioc/types';
+import { IClient } from '../../contracts';
+
+import { IIntent } from 'aleksa/IIntent';
+
+@injectable()
+export class TachankaBotIntent implements IIntent  {
+  name: string = "TachankaBot";
+
+  constructor(
+    @inject(TYPES.Logger) private _logger: ILogger,
+    @inject(TYPES.AleksaServerSelector) private _serverSelector: ServerSelectorService,
+    @inject(TYPES.IClient) private _client: IClient
+  ) { }
+
+  getCallback(config): (request: any, response: any) => Promise<void> {
+    return async (req, res) => {
+      const { guildId, channelId, userId, name } = await this._serverSelector.getServer();
+      this._client.sendMessage(guildId, channelId, `>sing for Arijoon`, {}, { isCommand: true });
+    };
+  }
+}
