b97db1f5caa8caaa4224fc3603da52b1880c8949
Arman Yaraee <arman@yaraee.net>
2018-11-30 20:28:30 +0000

    Moved log line after set

--- src/services/SessionManager.ts
+++ src/services/SessionManager.ts
@@ -31,8 +31,8 @@ export class SessionManager implements ISessionManager {
 
     const json = JSON.stringify(session);
     const key = makeSingleKey(session.id);
-    this._logger.info(`Generated single sessoon ${session.id}, timeout:${this.sessionDefaultExpiry}`);
     await this._cache.set(key, json, this.sessionDefaultExpiry);
+    this._logger.info(`Generated single sessoon ${session.id}, timeout:${this.sessionDefaultExpiry}`);
 
     return session;
   }
