ceb92731328be46a4a1fddf673ec1f10ff2b5c81
Arman Yaraee <arman@yaraee.net>
2020-09-06 10:39:05 +0100

    code clenup

--- src/commands/roll.command.ts
+++ src/commands/roll.command.ts
@@ -3,77 +3,70 @@ import { IClient } from '../contracts';
 import { injectable, inject } from 'inversify';
 import { TYPES } from '../ioc/types';
 import { commands } from '../static';
-import { MessageCollector } from 'discord.js';
-import { swearWords } from '../static';
 
-import * as path from 'path';
 
 @injectable()
 export class RollCommand implements ICommand {
 
-    _command: string = commands.roll;
-    _subscriptions: IDisposable[] = [];
+  _command: string = commands.roll;
+  _subscriptions: IDisposable[] = [];
 
-    _reg = /(\d)?d(\d+)/g
+  _reg = /(\d)?d(\d+)/g
 
-    constructor(
-        @inject(TYPES.IClient) private _client: IClient,
-        @inject(TYPES.IConfig) private _config: IConfig
-    ) { }
+  constructor(
+    @inject(TYPES.IClient) private _client: IClient  ) { } 
 
-    attach(): void {
-        this._subscriptions.push(this._client
-            .getCommandStream(this._command)
-            .subscribe(imsg => {
-                let msg = imsg.Message;
+  attach(): void {
+    this._subscriptions.push(this._client
+      .getCommandStream(this._command)
+      .subscribe(imsg => {
+        let msg = imsg.Message;
 
-                const content = msg.content.trim();
-                if (!content) {
-                    imsg.done();
-                    return;
-                }
+        const content = msg.content.trim();
+        if (!content) {
+          imsg.done();
+          return;
+        }
 
-                (new Promise((resolve, reject) => {
+        (new Promise((resolve) => {
 
-                    let result = [];
+          let result = [];
 
-                    let match = this._reg.exec(content);
+          let match = this._reg.exec(content);
 
-                    if (!match) {
-                        resolve(msg.channel.send("Bad format mofo", { reply: msg }));
-                    }
+          if (!match) {
+            resolve(msg.channel.send("Bad format mofo", { reply: msg }));
+          }
 
-                    while(match != null) {
+          while (match != null) {
 
-                        let num = parseInt(match[1]) || 1;
-                        let die = parseInt(match[2]);
+            let num = parseInt(match[1]) || 1;
+            let die = parseInt(match[2]);
 
-                        let res = `Rolling ${num} x d${die}:`
-                        for(let i = 0; i < num; i++) {
-                            res += ` |${this.roll(die)}|`;
-                        }
+            let res = `Rolling ${num} x d${die}:`
+            for (let i = 0; i < num; i++) {
+              res += ` |${this.roll(die)}|`;
+            }
 
-                        result.push(res);
+            result.push(res);
 
-                        match = this._reg.exec(content);
-                    }
+            match = this._reg.exec(content);
+          }
 
-                    resolve(msg.channel.send(result.join('\n'), { code: 'md' }));
+          resolve(msg.channel.send(result.join('\n'), { code: 'md' }));
 
-                })).then(res => {
-                    imsg.done();
-                }).catch(err => {
-                    imsg.done(err, true);
-                });
+        })).then(() => {
+          imsg.done();
+        }).catch(err => {
+          imsg.done('', err);
+        });
+      }));
+  }
 
-                
-            }));
-    }
+  private roll(die: number) {
+    let result = Math.floor(Math.random() * die);
 
-    private roll(die: number) {
-        let result = Math.floor(Math.random() * die);
-
-        return result;
-    }
+    return result;
+  }
 
 }
