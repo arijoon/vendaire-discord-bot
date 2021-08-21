8972beab46b0bf13fa72dd02d8acba4096102daf
Arman Yaraee <arman@yaraee.net>
2021-08-21 14:00:05 +0100

    Further sanitize user input within shell commands

--- src/helpers/security.ts
+++ src/helpers/security.ts
@@ -5,3 +5,11 @@
 export function cmdSanatize(input: string) {
   return input.replace(/(["'$`\\\/:])/g,'\\$1');
 }
+
+/**
+ * Sanatize a user input to be used within a shell command but allow links
+ * @param input string
+ */
+export function cmdSanitizeArgs(input: string) {
+  return input.replace(/(["'$`\\])/g,'\\$1');
+}
--- src/services/gallerydl.service.ts
+++ src/services/gallerydl.service.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'inversify';
 import { TYPES } from '../ioc/types';
-import { runCommand } from '../helpers';
+import { cmdSanitizeArgs, runCommand } from '../helpers';
 
 @injectable()
 export class GalleryDl {
@@ -9,7 +9,7 @@ export class GalleryDl {
   ) { }
 
   async fetchMediaLink(link: string, options: any = {}) {
-    return (await runCommand(`gallery-dl -g "${link}"`, this._logger))
+    return (await runCommand(`gallery-dl -g "${cmdSanitizeArgs(link)}"`, this._logger))
       .split('\n')
       .map(link => link.trim())
       .filter(link => link)
